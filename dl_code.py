702fbde5a041520741103a73493f1326e79c574d
Senne Vansant <91118511+VansantSenne@users.noreply.github.com>
2023-12-15 18:21:52 +0100

    Update dl_code.py

--- dl_code.py
+++ dl_code.py
@@ -95,7 +95,7 @@ uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
 if uploaded_file is not None:
     # Display the uploaded image
-    image_display = Image.open(BytesIO(uploaded_file.read()))
+    image_display = Image.open(uploaded)
     st.image(image_display, caption="Uploaded Image.", use_column_width=True)
 
     # Preprocess the image
